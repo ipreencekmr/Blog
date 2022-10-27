5aba420d329c1089b18ad7b88d0b39ab8fe0e3bf
Prince Sharma <prince.sharma@ibm.com>
2022-10-27 13:23:33 +0530

    shared library instroduced

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,27 @@
+import mongoose from 'mongoose';
 import { app } from './app';
 
-app.listen(3000, () => {
-    console.log('listening on port 3000!');
-});
+const start = async () => {
+
+    console.log('starting Auth...');
+
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI must be defined');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+    } catch (err) {
+        console.error(err);
+    }
+    console.log('connected to mongo db');
+    app.listen(3000, () => {
+        console.log('Listening on 3000!');
+    });
+}
+
+start();
